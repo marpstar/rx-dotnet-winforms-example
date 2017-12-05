dc1c07375e246b97c8c48ea76b4249b09dac8689
Cody Sand <cjsand@gmail.com>
2017-12-05 00:01:16 -0600

    initial commit of project.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RxNETExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(48, 35);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(222, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(48, 81);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(222, 173);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 328);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtSearch);
+            this.Name = "Form1";
+            this.Text = "RxJS Example";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Windows.Forms;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace RxNETExample
+{
+    public partial class Form1 : Form
+    {
+        private IDisposable _subscription1;
+        private IDisposable _subscription2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private IObservable<IEnumerable<string>> GetResults(string term)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://en.wikipedia.org/w/api.php");
+            return client.GetAsync($"?action=opensearch&format=json&search={term}")
+                .ToObservable()                                                         // convert the .NET Async (Task<T>) to an IObservable<T>
+                .SelectMany(response => response.Content.ReadAsStringAsync())           // now we can do cool stuff like flatten async calls
+                .Select(responseString => (JArray.Parse(responseString)[1] as JArray).Values<string>());   // and transform the response
+        }
+
+        private IDisposable PopulateListFromObservable(IObservable<IEnumerable<string>> ob)
+        {
+            return ob.ObserveOn(SynchronizationContext.Current)      // IMPORTANT: switch back to the UI thread, otherwise we'll get errors or see no updates.
+                .Subscribe(results =>
+                {                                               // finally, when we get the results back:
+                    listBox1.Items.Clear();                     // clear the list
+                    listBox1.Items.AddRange(results.ToArray()); // append the new results. 
+                });
+        }
+
+        private void Form1_Load(object sender, EventArgs ev)
+        {   
+            var keyup = Observable.FromEventPattern(txtSearch, nameof(KeyUp))     // listen to KeyUp events on the textbox
+                .Select(e => txtSearch.Text)                    // when it fires, grab the text from the textbox
+                .Where(text => text.Length > 2)                 // only continue if the term is > 2 characters
+                .Throttle(TimeSpan.FromMilliseconds(750))       // Throttle to every 750ms
+                .DistinctUntilChanged()                         // only fire if it's changed
+                .SelectMany(GetResults);                        // get the results from wikipedia
+
+
+            var doubleClick = Observable.FromEventPattern(listBox1, nameof(DoubleClick))
+                .Select(x => listBox1.SelectedItem.ToString())
+                .Do(x => txtSearch.Text = x)
+                .SelectMany(GetResults);
+                
+
+            _subscription1 = PopulateListFromObservable(keyup);
+            _subscription2 = PopulateListFromObservable(doubleClick);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _subscription1.Dispose();       // Rx.NET makes use of the IDisposable at the heart of .NET
+            _subscription2.Dispose();
+        }
+    }
+}
